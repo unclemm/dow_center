45a7419ce8dfbe604a2ccba06116e238d14e52c9
Hsiang Lin <hsiang.lin23@gmail.com>
2013-04-05 10:14:33 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DowCenter::Application.config.secret_token = '744e4c5638dea24b39a068b35f8dec19a9d22de858e10b85bd9bed6028fcc54c858300da49b4e6c1f7eaee659f7bb3d5317a866484aa96f30f67519287e6f85a'
