6865ee8246676e3e75067d502c5580dc0e4a02ab
Hsiang Lin <hsiang.lin23@gmail.com>
2013-04-19 13:57:16 +0000

    login working, booleans in place

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :position, :password, :password_confirmation
+  attr_accessible :name, :email, :position, :password, :password_confirmation, :regular, :supervisor, :lifeguard
   has_secure_password
 
   before_save { |user| user.email = email.downcase}
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,32 @@ DowCenter::Application.routes.draw do
   
   match '/signup', to: 'users#new'
 
-  root to: 'dow_center_pages#home'
+  match '/lifeguard', to: 'lifeguard_pages#home'
+  match '/supervisor', to: 'supervisor_pages#home'
+  match '/regular', to: 'regular_worker_pages#home'
+  match '/admin', to: 'admin_pages#home'
+  match '/home', to: 'dow_center_pages#home'
+
+
+##
+#  lifeguard_path => '/lifeguard'
+#  lifeguard_url => 'http://localhost:3000/lifeguard'
+
+#  supervisor_path => '/supervisor'
+#  supervisor_url => 'http://localhost:3000/supervisor'
+
+#  regular_path => '/regular'
+#  regular_url => 'http://localhost:3000/regular'
+
+#  admin_path => '/admin'
+#  admin_url => 'http://localhost:3000/admin'
+
+  root to: 'sessions#new'
+
 
   get "admin_pages/home"
   get "admin_pages/message"
- get  "admin_pages/worker_list"
+  get "admin_pages/worker_list"
   get "lifeguard_pages/home"
   get "lifeguard_pages/rules_and_protocols"
   get "supervisor_pages/home"
